3ab1926b6d9ab58cde4ffb8af7d8c920b661811d
Vctr <vctrmndz77@gmail.com>
2020-06-01 20:43:44 -0600

    header

---
+++ main.js
@@ -0,0 +1,8 @@
+//Selectors
+let header = document.querySelector('header');
+
+let hamburguerMenu = document.querySelector('.hamburguer-menu');
+
+hamburguerMenu.addEventListener('click', function() {
+	header.classList.toggle('menu-open');
+});
