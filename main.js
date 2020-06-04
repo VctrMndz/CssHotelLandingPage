b3a2ec766b65443073312f4d0870cb45cb8bbe30
Vctr <vctrmndz77@gmail.com>
2020-06-03 19:03:31 -0600

    Main page css ex

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ let header = document.querySelector('header');
 
 let hamburguerMenu = document.querySelector('.hamburguer-menu');
 
+window.addEventListener('scroll', function() {
+	let windowPosition = window.scrollY > 0;
+	header.classList.toggle('active', windowPosition);
+});
+
 hamburguerMenu.addEventListener('click', function() {
 	header.classList.toggle('menu-open');
 });
